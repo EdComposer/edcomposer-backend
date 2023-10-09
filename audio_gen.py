8e29c4ad61d04e51f9a72c9bfbd6c77eea06c584
dhanush17-tech <dhanush.kalaiselvan@gmail.com>
2023-10-09 16:52:52 -0700

    using async

--- audio_gen.py
+++ audio_gen.py
@@ -2,13 +2,15 @@ import os
 import boto3
 import asyncio
 from datetime import datetime
-from aiohttp import ClientSession
+from dotenv import load_dotenv
 
-AWS_DEFAULT_REGION = "your-aws-region"
-REMOTION_AWS_ACCESS_KEY_ID = "your-access-key-id"
-REMOTION_AWS_SECRET_ACCESS_KEY = "your-secret-access-key"
-AWS_SESSION_TOKEN = "your-session-token"
-BUCKET_NAME = "edcomposer"
+# Load the environment variables
+load_dotenv()
+
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
+REMOTION_AWS_ACCESS_KEY_ID = os.getenv("REMOTION_AWS_ACCESS_KEY_ID")
+REMOTION_AWS_SECRET_ACCESS_KEY = os.getenv("REMOTION_AWS_SECRET_ACCESS_KEY")
+AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
 
 session = boto3.Session(
     region_name=AWS_DEFAULT_REGION,
@@ -18,25 +20,32 @@ session = boto3.Session(
 )
 
 polly = session.client("polly", region_name=AWS_DEFAULT_REGION)
+s3 = boto3.client("s3", region_name=AWS_DEFAULT_REGION,
+                  aws_access_key_id=REMOTION_AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=REMOTION_AWS_SECRET_ACCESS_KEY,
+                  aws_session_token=AWS_SESSION_TOKEN)
 
-async def getAudio(textPrompt, session):
+async def get_audio(text_prompt):
     # Upload the text to AWS Polly and get the audio file url
-    response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3', Text=textPrompt)
-    file_path = "response.mp3"
-    async with session.get(response['AudioStream'].url) as response:
-        response.raise_for_status()
-        audio_data = await response.read()
+    response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3', Text=text_prompt)
     
     # Generate a unique object key
-    object_key = f"{datetime.timestamp(datetime.now())}-{textPrompt}.mp3"
+    object_key = f"{datetime.timestamp(datetime.now())}-{text_prompt}.mp3"
 
-    # Upload the audio data to S3
-    s3 = session.client("s3", region_name=AWS_DEFAULT_REGION)
+    # Save the audio from the response
+    file_path = f"audio/{object_key}"
+
+    with open(file_path, "wb") as file:
+        file.write(response["AudioStream"].read())
+
     try:
-        s3.put_object(Bucket=BUCKET_NAME, Key=object_key, Body=audio_data)
+        s3.upload_file(file_path, "edcomposer", object_key)
         
         # Generate the download URL for the uploaded file
-        s3_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{object_key}"
+        s3_url = f"https://edcomposer.s3.amazonaws.com/{object_key}"
+
+        # Delete the file from the local directory
+        os.remove(file_path)
         
         return s3_url
 
@@ -45,16 +54,15 @@ async def getAudio(textPrompt, session):
         return None
 
 async def process_batch(text_prompts):
-    async with ClientSession() as session:
-        tasks = [getAudio(prompt, session) for prompt in text_prompts]
-        audio_urls = await asyncio.gather(*tasks)
-        return audio_urls
+    tasks = [get_audio(prompt) for prompt in text_prompts]
+    audio_urls = await asyncio.gather(*tasks)
+    return audio_urls
 
 # Example usage:
 text_prompts = [
-    "Prompt 1",
-    "Prompt 2",
-    # Add more prompts here
+    "The Estates-General convened in 1789, leading to the formation of the National Assembly, representing the common people's interests.",
+    "The National Constituent Assembly (1789-1791) drafted the Constitution of 1791, establishing a constitutional monarchy.",
+    # Add more text prompts here
 ]
 
 loop = asyncio.get_event_loop()
