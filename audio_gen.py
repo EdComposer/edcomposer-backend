11b7eaa8862afa12d96db0ca92db34da419a814c
dhanush17-tech <dhanush.kalaiselvan@gmail.com>
2023-10-09 15:57:31 -0700

    added a way to asynchronously get audio urls from polly

--- audio_gen.py
+++ audio_gen.py
@@ -1,48 +1,42 @@
 import os
-import requests
-from dotenv import load_dotenv
 import boto3
+import asyncio
 from datetime import datetime
+from aiohttp import ClientSession
 
-#load the environment variables
-load_dotenv()
+AWS_DEFAULT_REGION = "your-aws-region"
+REMOTION_AWS_ACCESS_KEY_ID = "your-access-key-id"
+REMOTION_AWS_SECRET_ACCESS_KEY = "your-secret-access-key"
+AWS_SESSION_TOKEN = "your-session-token"
+BUCKET_NAME = "edcomposer"
 
-AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
-REMOTION_AWS_ACCESS_KEY_ID = os.getenv("REMOTION_AWS_ACCESS_KEY_ID")
-REMOTION_AWS_SECRET_ACCESS_KEY = os.getenv("REMOTION_AWS_SECRET_ACCESS_KEY")
-AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+session = boto3.Session(
+    region_name=AWS_DEFAULT_REGION,
+    aws_access_key_id=REMOTION_AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=REMOTION_AWS_SECRET_ACCESS_KEY,
+    aws_session_token=AWS_SESSION_TOKEN
+)
 
-
-session = boto3.Session(profile_name="dhanush")
 polly = session.client("polly", region_name=AWS_DEFAULT_REGION)
-s3 = boto3.client("s3", region_name=AWS_DEFAULT_REGION,
-                      aws_access_key_id=REMOTION_AWS_ACCESS_KEY_ID,
-                      aws_secret_access_key=REMOTION_AWS_SECRET_ACCESS_KEY,
-                      aws_session_token=AWS_SESSION_TOKEN)
-def getAudio(textPrompt):
-    #upload the text to aws polly and get the audio file url
-    polly = boto3.client("polly", region_name=AWS_DEFAULT_REGION, aws_access_key_id=REMOTION_AWS_ACCESS_KEY_ID, aws_secret_access_key=REMOTION_AWS_SECRET_ACCESS_KEY, aws_session_token=AWS_SESSION_TOKEN)
-    response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3', Text = textPrompt)
+
+async def getAudio(textPrompt, session):
+    # Upload the text to AWS Polly and get the audio file url
+    response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3', Text=textPrompt)
     file_path = "response.mp3"
-    file = open(file_path, 'wb')
-    file.write(response['AudioStream'].read())
-    file.close()
-    audio_url = uploadFileToS3(file_path)
-    return audio_url
-
-def uploadFileToS3(file_path):
+    async with session.get(response['AudioStream'].url) as response:
+        response.raise_for_status()
+        audio_data = await response.read()
     
-    bucket_name = "edcomposer"
-    object_key = str(datetime.timestamp(datetime.now())) + ".mp3"
+    # Generate a unique object key
+    object_key = f"{datetime.timestamp(datetime.now())}-{textPrompt}.mp3"
 
+    # Upload the audio data to S3
+    s3 = session.client("s3", region_name=AWS_DEFAULT_REGION)
     try:
-        s3.upload_file(file_path, bucket_name, object_key)
+        s3.put_object(Bucket=BUCKET_NAME, Key=object_key, Body=audio_data)
         
         # Generate the download URL for the uploaded file
-        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{object_key}"
-
-        # Delete the file from the local directory
-        os.remove(file_path)
+        s3_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{object_key}"
         
         return s3_url
 
@@ -50,7 +44,22 @@ def uploadFileToS3(file_path):
         print(f"Error uploading to S3: {e}")
         return None
 
- 
-audio_url = getAudio("The Estates-General convened in 1789, leading to the formation of the National Assembly, representing the common people's interests.")
-if audio_url:
-    print(f"Audio URL: {audio_url}")
+async def process_batch(text_prompts):
+    async with ClientSession() as session:
+        tasks = [getAudio(prompt, session) for prompt in text_prompts]
+        audio_urls = await asyncio.gather(*tasks)
+        return audio_urls
+
+# Example usage:
+text_prompts = [
+    "Prompt 1",
+    "Prompt 2",
+    # Add more prompts here
+]
+
+loop = asyncio.get_event_loop()
+audio_urls = loop.run_until_complete(process_batch(text_prompts))
+
+for i, url in enumerate(audio_urls):
+    if url:
+        print(f"Audio URL for Prompt {i + 1}: {url}")
