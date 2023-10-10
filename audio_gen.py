e2f26447e2af146dbd8e620872cc6596c8ddcb1f
Soham Daga <soham.daga22@gmail.com>
2023-10-09 23:47:16 -0700

    [ERRORED] using all of them together

---
+++ audio_gen.py
@@ -0,0 +1,76 @@
+import os
+import boto3
+import asyncio
+from datetime import datetime
+from dotenv import load_dotenv
+import aiofiles
+
+# Load the environment variables
+load_dotenv()
+
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
+REMOTION_AWS_ACCESS_KEY_ID = os.getenv("REMOTION_AWS_ACCESS_KEY_ID")
+REMOTION_AWS_SECRET_ACCESS_KEY = os.getenv("REMOTION_AWS_SECRET_ACCESS_KEY")
+AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+
+session = boto3.Session(
+    region_name=AWS_DEFAULT_REGION,
+    aws_access_key_id=REMOTION_AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=REMOTION_AWS_SECRET_ACCESS_KEY,
+    aws_session_token=AWS_SESSION_TOKEN
+)
+
+polly = session.client("polly", region_name=AWS_DEFAULT_REGION)
+s3 = boto3.client("s3", region_name=AWS_DEFAULT_REGION,
+                  aws_access_key_id=REMOTION_AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=REMOTION_AWS_SECRET_ACCESS_KEY,
+                  aws_session_token=AWS_SESSION_TOKEN)
+
+async def get_audio(text_prompt):
+    # Upload the text to AWS Polly and get the audio file url
+    response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3', Text=text_prompt)
+    
+    # Generate a unique object key
+    object_key = f"{datetime.timestamp(datetime.now())}.mp3"
+
+    # Save the audio from the response
+    file_path = f"audio/{object_key}"
+
+    async with aiofiles.open(file_path, "wb") as file:
+        await file.write(response["AudioStream"].read())
+
+    try:
+        s3.upload_file(file_path, "edcomposer", object_key)
+        
+        # Generate the download URL for the uploaded file
+        s3_url = f"https://edcomposer.s3.amazonaws.com/{object_key}"
+
+        # Delete the file from the local directory
+        os.remove(file_path)
+        
+        return s3_url
+
+    except Exception as e:
+        print(f"Error uploading to S3: {e}")
+        return None
+
+async def process_batch(text_prompts):
+    tasks = [get_audio(prompt) for prompt in text_prompts]
+    audio_urls = await asyncio.gather(*tasks)
+    return audio_urls
+
+
+async def generate_audio_urls(audio_prompts):
+    audio_url_list = await process_batch(audio_prompts)
+    return audio_url_list
+
+# # Example usage:
+# text_prompts = [
+#     "The Estates-General convened in 1789, leading to the formation of the National Assembly, representing the common people's interests.",
+#     "The National Constituent Assembly (1789-1791) drafted the Constitution of 1791, establishing a constitutional monarchy.",
+#     # Add more text prompts here
+# ]
+
+# for i, url in enumerate(audio_urls):
+#     if url:
+#         print(f"Audio URL for Prompt {i + 1}: {url}")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import json
+from audio_gen import generate_audio_urls
+import picture_gen
 import asyncio
 import httpx
 from fastapi import FastAPI
@@ -10,12 +12,10 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-topic = "World War II"
-
 app = FastAPI()
 
 @app.get("/video-data")
-async def generate_video_data():
+async def generate_video_data(prompt: str):
     audioGenerationLists = json.loads(openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
@@ -31,11 +31,10 @@ The format to be followed is [[<detail>,<detail>...],[<detail>,<detail>...]...].
             },
             {
                 "role": "user",
-                "content": topic
+                "content": prompt
             }
         ]
     )["choices"][0]["message"]["content"])
-
 
     imageGenerationLists = []
     for audioGenerationList in audioGenerationLists:
@@ -48,8 +47,8 @@ The format to be followed is [[<detail>,<detail>...],[<detail>,<detail>...]...].
                              THE MOST IMPORTANT THING IS TO RETURN ONLY TEXT THAT CAN BE PARSED AS JSON, IT NEEDS TO BE A VALID JSON STRING SO DO NOT INCLUDE ANY OTHER EXTRA INFORMATION AT ALL.
                              You are an array generator that will receive an array of strings and generate NOTHING but an array with nested dictionaries (a single dictionary for each string in the input list).
                              Each dictionary should have a boolean field called generate and a string field called prompt.
-                             The generate field should be false if the image can easily be found online and true otherwise.
-                             If generate is true then the prompt field should contain a detailed prompt from the input string for image generation, otherwise the prompt field will contain a simplified string for google search.
+                             The generate field should be python boolean False if the image can easily be found online OR if it violates policy and python boolean True otherwise.
+                             If generate is true then the prompt field should contain a detailed prompt (that follows DALL-E policy) from the input string for image generation, otherwise the prompt field will contain a simplified string for google search.
                              GENERATE THE EXACT SAME AMOUNT OF DICTIONARIES AS THE AMOUNT OF STRINGS IN EACH LIST.
                             The format to be followed is [{'generate': <True/False>, 'prompt':<prompt string>}, {'generate': <True/False>, 'prompt':<prompt string>},...]. The input array is 
                             '''
@@ -68,11 +67,18 @@ The format to be followed is [[<detail>,<detail>...],[<detail>,<detail>...]...].
 
     videoData = ""
     for i in range(0, len(audioGenerationLists)):
+        audioUrlList = asyncio.run(generate_audio_urls(audioGenerationLists[i]))
+        imageUrlList = picture_gen.generatePictures(imageGenerationLists[i])
         currentData = ""
+
         for j in range(0, len(audioGenerationLists[i])):
-            currentData += str({'caption': json.loads(audioGenerationLists)[i][j], 'audio': json.loads(audioGenerationLists)[i][j], 'image': imageGenerationLists[i][j]})
+            currentData += str({'caption': audioGenerationLists[i][j], 'audio': audioUrlList[j], 'image': imageUrlList[j]})
+        
         videoData += "{" + currentData + "},"
+
     videoData = "{" + videoData + "}"
+
+    print(videoData)
     
     return videoData
 
